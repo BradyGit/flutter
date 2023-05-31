d96e330da24e272ea461ec389082b541e1d4fbe5
brady <back2moons@gmail.com>
2023-06-01 00:24:33 +0900

    앱 테마, 칼라 설정.

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,17 @@ import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:mbti/router/router.dart';
 import 'package:mbti/styles/theme.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
   log('main');
-  runApp(MaterialApp.router(
-    theme: theme,
-    routerConfig: router,
-  ));
+
+  runApp(
+    ProviderScope(
+      child: MaterialApp.router(
+        theme: theme,
+        routerConfig: router,
+      ),
+    ),
+  );
 }
--- lib/router/screens/home_screen.dart
+++ lib/router/screens/home_screen.dart
@@ -1,18 +1,47 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class HomeScreen extends StatelessWidget {
+final futureProvider = FutureProvider<int>((ref) {
+  return Future.delayed(const Duration(seconds: 3), () => 5);
+});
+
+final streamProvider = StreamProvider<int>((ref) {
+  int count = 0;
+  return Stream.periodic(const Duration(seconds: 2), (_) => count++);
+});
+
+class HomeScreen extends ConsumerWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    log('HomeScreen');
-    return Container(
-      color: Colors.amber,
-      width: double.infinity,
-      height: double.infinity,
-      child: const Text('Home Screen'),
+  Widget build(BuildContext context, WidgetRef ref) {
+    final streamAsyncValue = ref.watch(streamProvider);
+
+    final futureAsyncValue = ref.watch(futureProvider);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('HomePage'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Center(
+            child: streamAsyncValue.when(
+              data: (data) => Text('Value: $data'),
+              loading: () => const CircularProgressIndicator(),
+              error: (e, st) => Text('Error: $e'),
+            ),
+          ),
+          Center(
+            child: futureAsyncValue.when(
+              data: (data) => Text('Value: $data'),
+              loading: () => const CircularProgressIndicator(),
+              error: (e, st) => Text('Error: $e'),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
